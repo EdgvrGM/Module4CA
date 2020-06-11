5394d7a78ab4aecdb9d81a1d02150d58cfbaeeb1
Edgar Gonzalez <edgar_gm6@hotmail.com>
2020-06-11 13:55:36 -0500

    solution

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 (function () {
   var names = [
-    "Jose",
-    "Juan",
-    "Jordan",
-    "Travis",
+    "Yaakov",
+    "John",
+    "Jen",
+    "Jason",
     "Paul",
-    "Francis",
+    "Frank",
     "Larry",
-    "Niko",
-    "Ricardo",
-    "Hugo",
+    "Paula",
+    "Laura",
+    "Jim",
   ];
   for (var i = 0; i < names.length; i++) {
     var firstLetter = names[i].charAt(0).toLowerCase();
